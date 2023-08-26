95e5efdb349ab962a5e21530757c682ef9b3593d
Artem Khusnullin <twentyfortythree@yandex.ru>
2023-08-26 19:34:08 +0500

    Добавил корсы

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'https://twentyfortythree.nomoreparties.co',
+  'http://twentyfortythree.nomoreparties.co',
+];
+
 const { PORT = 3000, DB_ADDRESS } = process.env;
 
 const app = express();
@@ -17,6 +22,23 @@ app.use(express.urlencoded({ extended: true }));
 mongoose.connect(process.env === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1/bitfilmsdb');
 
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
 
 app.use(router);
 
